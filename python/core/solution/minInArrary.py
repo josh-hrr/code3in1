63a69ee402b0ac83da888b64c0a895965de822b9
Josue Herrera <54568984+josh-hrr@users.noreply.github.com>
2023-12-06 23:14:55 -0600

    Create minInArrary.py

---
+++ python/core/solution/minInArrary.py
@@ -0,0 +1,16 @@
+array = [1, 100, 3, 50, 15]
+
+# Find the first minimum number
+first_min = min(array)
+index_to_remove = array.index(first_min)
+array.pop(index_to_remove)
+
+# Print the modified array
+print(array)
+
+# Find the second minimum number
+second_min = min(array)
+
+# Print the results
+print("firstMin:", first_min)
+print("secondMin:", second_min)
