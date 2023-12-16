2e558573f57c9bf559b499f7bab94078a4ec10c5
Josue Herrera <54568984+josh-hrr@users.noreply.github.com>
2023-12-16 00:11:55 -0600

    Create reversedString.java

---
+++ java/core/solution/reversedString.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Kata {
+
+  public static String solution(String str) {
+    StringBuilder input = new StringBuilder();
+    input.append(str);
+    return String.valueOf(input.reverse()); 
+  }
+
+}
